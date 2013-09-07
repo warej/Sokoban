1ccbfb807f4c08bb6ba1142cffd16c04586edcbe
warej <awarejko@wp.pl>
2013-09-07 12:27:53 +0200

    Poprawka formatowania w loggerze.

--- logger.js
+++ logger.js
@@ -22,14 +22,14 @@ Logger.prototype.w = function(msg) {
     var log = document.getElementById("logs").innerHTML;
     //log = msg + log;
     document.getElementById("logs").innerHTML =
-        this.time() + "WARN  " + msg + "<br />" + log;
+        this.time() + "WARN &nbsp;" + msg + "<br />" + log;
 };
 
 Logger.prototype.i = function(msg) {
     var log = document.getElementById("logs").innerHTML;
     //log = msg + log;
     document.getElementById("logs").innerHTML =
-        this.time() + "INFO  " + msg + "<br />" + log;
+        this.time() + "INFO  &nbsp;" + msg + "<br />" + log;
 };
 
 Logger.prototype.d = function(msg) {
